7a9009bdae7b02e173fed339ea6cdb4de90ad5cc
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2020-07-19 20:07:11 +0700

    fix(depedency): remove unused depedency

--- chat-bot/app.py
+++ chat-bot/app.py
@@ -1,11 +1,10 @@
+import time
 import telepot
 import logging
 import requests
 import threading
 import logging.config
 
-from telegram.ext import ParseMode
-from queue import Queue
 from telepot.loop import MessageLoop
 
 logging.config.fileConfig('public/config/logging.config.ini')
@@ -15,8 +14,9 @@ def handle(msg):
     command = ''
     content_type, chat_type, chat_id = telepot.glance(msg)
     logging.info('----------Got Connection From User [{}]-------'.format(chat_id))
-    file_name = get_filename(str(chat_id))
-    img_path = IMG_PATH + file_name
+
+    if 'text' in msg:
+        command = msg['text']
 
     logging.info('content_type is: {}'.format(content_type))
     logging.info('chat_id is: {}'.format(chat_id))
@@ -29,8 +29,10 @@ def handle(msg):
 
 if __name__ == "__main__":
     bot = telepot.Bot('1134714364:AAGubrGz9t9RP_Y7NUF0taViVT68w9nHhwE')
-    queue_1 = Queue()
     MessageLoop(bot, handle).run_as_thread()
 
-    t2 = threading.Thread(target=send_recv_img, args=(queue_1), name='Thread 2')
-    t2.start()
+    print ('Listening ...')
+
+    # Keep the program running.
+    while 1:
+        time.sleep(10)
